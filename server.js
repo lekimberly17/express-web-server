4cd0d4db95b0f21823efb1de9836a67934b771c3
lekimberly17 <lekimberly509@gmail.com>
2023-03-06 10:56:18 -0800

    server

--- server.js
+++ server.js
@@ -1,1 +1,9 @@
+const express = require("express");
+const app = express();
+const port = 3000;
 
+app.use(express.static("public")); // serving 
+
+app.listen(port, () => {
+    console.log(`Server listening at http://localhost:${port}`)
+})
